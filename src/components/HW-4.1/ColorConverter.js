45d31c440ee895c4428d71b1bbeec189e556b178
NazarovAn <naatonchrome@gmail.com>
2020-11-28 14:25:43 +0400

    changes to color converter

--- src/components/HW-4.1/ColorConverter.js
+++ src/components/HW-4.1/ColorConverter.js
@@ -4,28 +4,36 @@ import Input from './Input/Input'
 import Output from './Output/Output'
 
 export default function ColorConverter() {
-  const [state, setState] = useState({ color: '#ddeeee', output: 'rgb(221, 238, 238)' });
-  const checkInputValue = (val) => {    
-    if (val.length !== 7) {
-      return;
-    }
-    const regex = /^#([A-Fa-f0-9]+)$/;  
+  const [color, setColor] = useState({ hex: '#ddeeee', rgb: 'rgb(221, 238, 238)' });
 
-    if (regex.test(val)) {
-      const r = parseInt(val.slice(1,3), 16);
-      const g = parseInt(val.slice(3,5), 16);
-      const b = parseInt(val.slice(5,7), 16);
-      const color = `rgb(${r}, ${g}, ${b})`;
-      setState((prev) => ({ ...prev, color, output: color }));
-    } else {
-      setState((prev) => ({ ...prev, color: '#ff4444', output: 'Ошибка!' }));
-    }
+  const isValid = (value) => {
+    return /^#([A-Fa-f0-9]+)$/.test(value) ? true : false
   }
+  
+  const convertHexToRgb = (value) => {
+    const r = parseInt(value.slice(1,3), 16);
+    const g = parseInt(value.slice(3,5), 16);
+    const b = parseInt(value.slice(5,7), 16);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  const handleInput = (value) => {
+    if (value.length !== 7 ) {
+      return null
+    }
+
+    if (!isValid(value)) {
+      return setColor((prev) => ({ ...prev, hex: '#ff4444', rgb: 'Ошибка!' }));
+    }
+    
+    setColor((prev) => ({ ...prev, hex: value, rgb: convertHexToRgb(value) }));
+  }
+
   return (
-    <div className="color_converter__wrapper" style={{ backgroundColor: state.color }}>
+    <div className="color_converter__wrapper" style={{ backgroundColor: color.hex }}>
       <div className="color_converter__form">
-        <Input onChange={ checkInputValue } />
-        <Output output={ state.output } />
+        <Input onChange={ handleInput } />
+        <Output output={ color.rgb } />
       </div>
     </div>
   )
--- src/components/HW-4.1/Input/Input.js
+++ src/components/HW-4.1/Input/Input.js
@@ -4,7 +4,7 @@ import './Input.css'
 export default function Input(props) {
   return (
     <React.Fragment>
-      <input className="color_converter__input" onChange={ (ev) => props.onChange(ev.target.value) } maxLength="7" />
+      <input className="color_converter__input" onChange={ (event) => props.onChange(event.target.value) } maxLength="7" />
     </React.Fragment>
   )
 }
